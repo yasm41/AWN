c4b66f853d0f43f5d2e007be55efc22da71ee545
Yasmin <yasmin@example.com>
2026-06-22 22:57:58 +0200

    fix IPv4 SMTP

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,6 +5,7 @@ const createTransporter = () => {
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT || "587", 10),
     secure: process.env.SMTP_SECURE === "true",
+    family:4,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
